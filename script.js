847e1afe247e2d34b1c4874918993cbbd84ac7b7
MikixIT <michael.46@live.it>
2023-01-24 17:24:34 +0100

    fixing stuff

---
+++ script.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: 1,
+    question: "what was Super Mario's first real name?",
+    answears: [
+      { answear: "Jumpman", isCorrect: true },
+      { answear: "Mega Mario", isCorrect: false },
+      { answear: "Super Dario", isCorrect: false },
+      { answear: "Jumpboy", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "what was Super Mario's first real name?",
+    answears: [
+      { answear: "A", isCorrect: true },
+      { answear: "B", isCorrect: false },
+      { answear: "C", isCorrect: false },
+      { answear: "D", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "what was Super Mario's first real name?",
+    answears: [
+      { answear: "A", isCorrect: true },
+      { answear: "B", isCorrect: false },
+      { answear: "C", isCorrect: false },
+      { answear: "D", isCorrect: false },
+    ],
+  },
+];
